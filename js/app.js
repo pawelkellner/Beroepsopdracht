27733d49afebb3e24aa83dfef947064da9659d5e
Paweł Kellner <karkuwa@Pawel-MacBook.local>
2021-01-13 16:03:24 +0100

    directory gefixed

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ const controls = new Controls(50);
 controls.enableMovement();
 
 // create a model
-const melkMeisje = new Model('assets/models/vermeer/export_museumprops.babylon.gltf');
+const melkMeisje = new Model('../assets/models/vermeer/export_museumprops.babylon.gltf');
 melkMeisje.setPosition(-0.2, -0.1, 0.0);
 melkMeisje.setScale(100, 100, 100);
 melkMeisje.setRotation(0, 45.45, 0);
 
 // create a model
-const cuberoom = new Model('assets/models/cube-room/cube-room.gltf');
+const cuberoom = new Model('../assets/models/cube-room/cube-room.gltf');
 cuberoom.setPosition(0, 0, 0);
 cuberoom.setScale(-.5, 0.5, 0.5);
 cuberoom.setRotation(-0.76, -36.95, 0);
@@ -29,24 +29,24 @@ lamText.setScale(0.5, 0.5, 0.5)
 lamText.setRotation(0, -38, 0);
 
 // create an image
-const image1 = new XRImage('assets/images/Lisse-049.jpg');
+const image1 = new XRImage('../assets/images/Lisse-049.jpg');
 image1.setPosition(0, 1.8, 1.5);
 image1.setRotation(-0.74, 43, 178);
 image1.setScale(1.5, 1.5, 1.5);
 // create an imageí
-const image4 = new XRImage('assets/images/eettafel.jpg');
+const image4 = new XRImage('../assets/images/eettafel.jpg');
 image4.setPosition(0, 2, -3);
 image4.setRotation(-0.74, 43, 1);
 image4.setScale(1.5, 1.5, 1.5);
 
 // create an imageí
-const image2 = new XRImage('assets/images/FolkertdeJong.jpg');
+const image2 = new XRImage('../assets/images/FolkertdeJong.jpg');
 image2.setPosition(0, 2, -3);
 image2.setRotation(0, -39, -1);
 image2.setScale(1.5, 1.5, 1.5);
 
 // create an imageí
-const image3 = new XRImage('assets/images/Lisse-043.jpg');
+const image3 = new XRImage('../assets/images/Lisse-043.jpg');
 image3.setPosition(2.1, 1.9, 0.7);
 image3.setRotation(0, -127, 0);
 image3.setScale(1.5, 1.5, 1.5);
@@ -55,7 +55,7 @@ image3.setScale(1.5, 1.5, 1.5);
 
 
 // create a 360 image
-const image360 = new Sky('assets/LamIngang.jpg');
+const image360 = new Sky('../assets/LamIngang.jpg');
 
 const spotLight = new SpotLight();
 spotLight.setPosition(-1, 2, 3.7);
