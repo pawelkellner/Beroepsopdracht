9d7cbb09fa275decbd6588bc2f3e38ed05424ca1
Pawel <smrpawel@gmail.com>
2021-01-15 17:47:59 +0100

    Informatie + positie van beelden verandert

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ controls.enableMovement();
 
 // create a model
 const melkMeisje = new Model('../assets/models/vermeer/export_museumprops.babylon.gltf');
-melkMeisje.setPosition(-0.2, -0.1, 0.0);
+melkMeisje.setPosition(-0.419, -0.1, -2.813);
 melkMeisje.setScale(100, 100, 100);
 melkMeisje.setRotation(0, 45.45, 0);
 
@@ -30,24 +30,24 @@ lamText.setRotation(0, -38, 0);
 
 // create an image
 const image1 = new XRImage('../assets/images/Lisse-049.jpg');
-image1.setPosition(0, 1.8, 1.5);
-image1.setRotation(-0.74, 43, 178);
+image1.setPosition(1.349, 1.8, 2.267);
+image1.setRotation(-0.74, 50, 178);
 image1.setScale(1.5, 1.5, 1.5);
 // create an imageí
 const image4 = new XRImage('../assets/images/eettafel.jpg');
-image4.setPosition(0, 2, -3);
+image4.setPosition(-2.019, 2.056, -0.902);
 image4.setRotation(-0.74, 43, 1);
 image4.setScale(1.5, 1.5, 1.5);
 
 // create an imageí
 const image2 = new XRImage('../assets/images/FolkertdeJong.jpg');
-image2.setPosition(0, 2, -3);
-image2.setRotation(0, -39, -1);
+image2.setPosition(-0.950, 2.147, -2.427);
+image2.setRotation(0, 50, -1);
 image2.setScale(1.5, 1.5, 1.5);
 
 // create an imageí
 const image3 = new XRImage('../assets/images/Lisse-043.jpg');
-image3.setPosition(2.1, 1.9, 0.7);
+image3.setPosition(2.01, 1.973, 0.761);
 image3.setRotation(0, -127, 0);
 image3.setScale(1.5, 1.5, 1.5);
 
@@ -63,17 +63,21 @@ spotLight.setTarget(melkMeisje);
 
 // listen for an event
 image1.addEventListener('mouseenter', () => 
-    document.getElementById('extraText').innerHTML= "Kunstwerk ‘Food Chain Project’ van kunstenaar Itamar Gilboa.");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie: Kunstwerk ‘Food Chain Project’ van kunstenaar Itamar Gilboa.");
 image1.addEventListener('mouseleave', () => 
-    document.getElementById('extraText').innerHTML= "");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie:");
 image2.addEventListener('mouseenter', () => 
-    document.getElementById('extraText').innerHTML= "Werkplaats van kunstenaar Folkert de Jong");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie: Werkplaats van kunstenaar Folkert de Jong");
 image2.addEventListener('mouseleave', () => 
-    document.getElementById('extraText').innerHTML= "");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie:");
 image3.addEventListener('mouseenter', () => 
-    document.getElementById('extraText').innerHTML= "Kunstwerk van kunstenaar Sabine Tol");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie: Kunstwerk van kunstenaar Sabine Tol");
 image3.addEventListener('mouseleave', () => 
-    document.getElementById('extraText').innerHTML= "");
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie:");
+image4.addEventListener('mouseenter', () => 
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie: Het Laatste Avondmaal in Teylers");
+image4.addEventListener('mouseleave', () => 
+    document.getElementById('extraText').innerHTML= "Kunstwerk informatie:");
 
 melkMeisje.addEventListener('click', () => 
-    document.getElementById('extraText').innerHTML = "Melkmeisje van Vermeer");
+    document.getElementById('extraText').innerHTML = "Kunstwerk informatie: Melkmeisje van Vermeer");
